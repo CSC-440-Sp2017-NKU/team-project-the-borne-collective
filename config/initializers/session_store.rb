6a1a6e04400c88b9fd82f70f982a1eb3000abd28
Corey Shrader <corey.shrader@yahoo.com>
2017-03-15 19:25:30 -0400

    postgres

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_nkunet_session'
+Rails.application.config.session_store :cookie_store, key: '_The_Borne_Collective_session'
