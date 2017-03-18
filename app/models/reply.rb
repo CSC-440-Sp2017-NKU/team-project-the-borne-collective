4f4309eb30f213d8ecf8ab4db0fd003dfd3d53d3
Jessee Meadows <jesseemeadows@gmail.com>
2017-03-18 02:57:55 +0000

    Add reply test

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,3 +1,5 @@
 class Reply < ApplicationRecord
     belongs_to :post
+    validates :content, presence: true, length:  { minimum: 8 }
+    validates :post_id, presence: true
 end
--- test/models/reply_test.rb
+++ test/models/reply_test.rb
@@ -14,4 +14,27 @@ class ReplyTest < ActiveSupport::TestCase
     assert @reply.valid?
   end
   
+  test "content can't be blank" do
+    @reply.content = " " * 15
+    assert_not @reply.valid?
+  end
+  
+  test "post_id must be valid" do
+    @reply.post_id = nil
+    assert_not @reply.valid?
+    if Post.first != nil
+      @reply.post_id = Post.first.id
+      assert @reply.valid?
+    end
+  
+  end
+  
+  test "content should be atleast 8 characters" do
+    @reply.content = "a" * 7
+    assert_not @reply.valid?
+    
+    @reply.content = "a" * 8
+    assert @reply.valid?
+  end
+  
 end
