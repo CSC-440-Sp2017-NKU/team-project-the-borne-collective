87690c5a5df85e7b01d18f8e96d36c8d7204fede
Jessee Meadows <jesseemeadows@gmail.com>
2017-04-13 15:12:35 +0000

    refactor views with shims courtesy of railstutorial.org/book

--- Gemfile
+++ Gemfile
@@ -9,6 +9,10 @@ ruby '2.4.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 
 gem 'rails', '~> 5.0.2'
+gem 'bcrypt',  '3.1.11'
+gem 'faker',                   '1.6.6'
+gem 'will_paginate',           '3.1.0'
+gem 'bootstrap-will_paginate', '0.0.10'
 
 #
 gem 'bootstrap-sass', '3.3.6'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,13 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
 end
