604607516f2ce1b500943990826517aad752cff2
Corey Shrader <shraderc1@nku.edu>
2017-04-17 04:16:53 +0000

    Implement cookie-based user authentication.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416225203) do
+ActiveRecord::Schema.define(version: 20170417024920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,13 +63,11 @@ ActiveRecord::Schema.define(version: 20170416225203) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.boolean  "admin",           default: false
-    t.boolean  "registrar",       default: false
-    t.string   "password_digest"
-    t.string   "remember_digest"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.string   "remember_digest"
+    t.boolean  "admin",           default: false
+    t.string   "password_digest"
   end
 
 end
