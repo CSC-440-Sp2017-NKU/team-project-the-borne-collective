3aee782e9d2aea2c47281e2144197d51e1ceca7b
Nathen Lyman <nathenlyman@gmail.com>
2017-04-19 00:34:12 +0000

    Fixed home page

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,7 @@ class Vote < ApplicationRecord
     belongs_to :post
     validates :post_id,   presence: true
     #validates :user_id,   presence: true
-    validates :vote,   presence: true, length:  { minimum: -1, maximum: 1 }
+    validates :vote,   presence: true
     
     def vote(post_id, user_id, num)
         Votes.create(:post_id => post_id, :user_id => user_id, :vote => num)
@@ -11,7 +11,7 @@ class Vote < ApplicationRecord
     # Doesnt do anything
     def count_votes(post_id)
         tot_vote = 0;
-        rs = con.exec "SELECT * FROM votes WHERE user_id = ''"
+        rs = con.exec "SELECT * FROM votes WHERE post_id = ''"
         Votes.vote.each do |x| 
             tot_vote = tot_vote + x
         end
