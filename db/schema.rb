89a69966b078a77de16639029cbbedd1b41ca0e4
Nathen Lyman <nathenlyman@gmail.com>
2017-04-20 02:04:29 +0000

    Fixed DB and missing bracket on index

--- db/schema.rb
+++ db/schema.rb
@@ -15,47 +15,26 @@ ActiveRecord::Schema.define(version: 20170418235639) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "course_records", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "course_id"
-    t.integer "status"
-    t.index ["course_id"], name: "index_course_records_on_course_id", using: :btree
-    t.index ["user_id"], name: "index_course_records_on_user_id", using: :btree
-  end
-
-  create_table "courses", force: :cascade do |t|
-    t.string   "name"
+  create_table "forums", force: :cascade do |t|
+    t.string   "subject"
     t.text     "description"
-    t.string   "titleline"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
   create_table "posts", force: :cascade do |t|
-    t.text     "content"
     t.string   "title"
-    t.integer  "user_id"
-    t.integer  "course_id"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["course_id"], name: "index_posts_on_course_id", using: :btree
-    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
+    t.integer  "forum_id"
   end
 
   create_table "replies", force: :cascade do |t|
     t.text     "content"
-    t.integer  "post_id"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_replies_on_post_id", using: :btree
-    t.index ["user_id"], name: "index_replies_on_user_id", using: :btree
-  end
-
-  create_table "votes", force: :cascade do |t|
-    t.integer "post_id"
-    t.integer "user_id"
-    t.integer "vote"
+    t.integer  "post_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -68,6 +47,12 @@ ActiveRecord::Schema.define(version: 20170418235639) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.integer "vote"
   end
 
 end
