0cbac68f856e29a8380a341f2b6815594e8783e7
JesseeMeadows <jesseemeadows@gmail.com>
2017-04-20 02:22:50 +0000

    finish course registration

--- app/models/course_record.rb
+++ app/models/course_record.rb
@@ -1,5 +1,11 @@
 class CourseRecord < ApplicationRecord
+   
     belongs_to :course
     belongs_to :user
+    enum status: [:enrolled, :completed, :teaching, :taught]
+     
+    validates :status, presence: true
+    validates_uniqueness_of :user_id, :scope => [:course_id]
+
     
 end
