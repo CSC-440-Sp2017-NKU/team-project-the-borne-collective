3336a2018da278c2bce20403d3df555af1813ad3
JesseeMeadows <jesseemeadows@gmail.com>
2017-05-03 23:19:39 +0000

    include zero search result message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  
+    before_action :logged_in_user, only: [:search]
   # [todo] Dynamically pass top Post based on rating when implemented
   def index
     @posts = Post.all
@@ -7,10 +7,22 @@ class HomeController < ApplicationController
   end
   
   def search
-    @query = Post.search do
+    query = Post.search do
         fulltext params[:q]
     end
-    @products = @query.results
+    @results = query.results
   end
   
+  private
+  
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    
 end
