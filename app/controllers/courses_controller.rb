1e787d50470a530cb6efa1a9e119955e1801fc37
Corey Shrader <corey.shrader@yahoo.com>
2017-05-03 23:03:07 -0400

    Fix course creation

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -71,6 +71,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:subject, :description)
+      params.require(:course).permit(:name, :description, :titleline)
     end
 end
