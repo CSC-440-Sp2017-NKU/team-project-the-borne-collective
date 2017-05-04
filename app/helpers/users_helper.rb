8722321d80c5d25b68f86e940f0662bee9820d99
JesseeMeadows <jesseemeadows@gmail.com>
2017-05-04 06:52:01 +0000

    update profile page(stills needs paginate)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,35 @@
 module UsersHelper
 
   # Returns the Gravatar for the given user.
-  def gravatar_for(user, size: 80)
+  def gravatar_for(user, size: 80, img_class: "gravatar")
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: user.name, class: img_class)
+  end
+  
+  # Returns a nicely formated date  --example: April 25, 2017  
+  def humanize_join_date(date_time)
+     return date_time.strftime("%B %d, %Y") 
+  end
+  
+  # Returns reputation status tag
+  def reputation_for(upvotes, downvotes) 
+    if downvotes == 0
+      return content_tag(:h4, "Good Poster!", style: "color:green")
+    elsif upvotes < 5
+      return content_tag(:h4, "New Poster", style: "color:green")
+    else
+      vote_ratio = downvotes / upvotes.to_f
+      if vote_ratio < 0.1
+        return content_tag(:h4, "Great Poster!", style: "color: green")
+      elsif vote_ratio < 0.18 
+        return content_tag(:h4, "Good Poster", style: "color:#5fba7d")
+      elsif vote_ratio < 0.25
+        return content_tag(:h4, "Controversial Poster", style: "color:goldrod")
+      else
+        return content_tag(:h4, "Bad Poster", style: "color:red")
+      end
+    end
   end
   
 end
